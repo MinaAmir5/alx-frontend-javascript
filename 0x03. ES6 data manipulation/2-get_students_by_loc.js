2792e865ba9b99b7f8cbe156bae329fae0381ea6
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2024-07-03 05:24:04 +0200

    Delete 0x03. ES6 data manipulation directory

--- 0x03. ES6 data manipulation/2-get_students_by_loc.js
+++
@@ -1,3 +0,0 @@
-export default function getStudentsByLocation(students, city) {
-  return students.filter((obj) => obj.location === city);
-}
--- 0x03. ES6 data manipulation/3-get_ids_sum.js
+++
@@ -1,3 +0,0 @@
-export default function getStudentIdsSum(students) {
-  return students.reduce((acc, c) => acc + c.id, 0);
-}
